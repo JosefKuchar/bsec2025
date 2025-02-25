5598af227691b290e719327368dce1fd93547aa0
Josef Kuchař <josef.kuchar267@gmail.com>
2025-02-25 17:03:37 +0100

    setup shadcn

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,25 @@
+<script lang="ts">
+	import Button from '$lib/components/ui/button/button.svelte';
+	import CardContent from '$lib/components/ui/card/card-content.svelte';
+	import CardDescription from '$lib/components/ui/card/card-description.svelte';
+	import CardFooter from '$lib/components/ui/card/card-footer.svelte';
+	import CardHeader from '$lib/components/ui/card/card-header.svelte';
+	import CardTitle from '$lib/components/ui/card/card-title.svelte';
+	import Card from '$lib/components/ui/card/card.svelte';
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<Button variant="secondary">Click me</Button>
+<Card>
+	<CardHeader>
+		<CardTitle>Card Title</CardTitle>
+		<CardDescription>Card Description</CardDescription>
+	</CardHeader>
+	<CardContent>
+		<p>Card Content</p>
+	</CardContent>
+	<CardFooter>
+		<p>Card Footer</p>
+	</CardFooter>
+</Card>
