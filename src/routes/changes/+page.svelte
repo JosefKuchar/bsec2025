ea94744e7095a604de04ceed8f8072435e7bd42d
DaveV6 <bujzasdavid@gmail.com>
2025-02-27 13:30:09 +0100

    prepare changes page

--- src/routes/changes/+page.svelte
+++ src/routes/changes/+page.svelte
@@ -1,25 +1,13 @@
 <script lang="ts">
-	import type { PageData } from './$types';
+	import DataTable from "./data-table.svelte";
+	import Dialog from "./dialog.svelte"
+	import { columns } from "./columns.js";
+	import type { PageData } from "../$types";
+	
 	export let data: PageData;
-	let changes = data.changes;
 </script>
-
-<svelte:head>
-	<title>Changes</title>
-	<meta name="description" content="List of changes" />
-</svelte:head>
-
-<div class="container mx-auto py-8">
-	<h1 class="text-3xl font-bold mb-4">Changes</h1>
-
-	<!-- Placeholder for the list -->
-	{#if changes.length === 0}
-		<p>No changes to display.</p>
-	{:else}
-		<ul class="list-inside">
-			{#each changes as change}
-				<li class="mb-2">{change.typeId} {change.amount} {change.frequency}</li>
-			{/each}
-		</ul>
-	{/if}
+	
+<div class="container">
+	<DataTable data={data.changes} {columns} />
+  <Dialog />
 </div>
