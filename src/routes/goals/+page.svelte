139c6f790d3ea89f75078436c89d18557d1cca8b
Roman <xjanot04@stud.fit.vutbr.cz>
2025-02-27 18:01:02 +0100

    style: enhance styling of New Goal button and dialog

--- src/routes/goals/+page.svelte
+++ src/routes/goals/+page.svelte
@@ -11,19 +11,22 @@
   
   <div class="space-y-4">
 	<Dialog.Root>
-	  <Dialog.Trigger>New Goal</Dialog.Trigger>
-	  <Dialog.Content>
-		<Dialog.Header>
-		  <Dialog.Title>New Goal</Dialog.Title>
-		  <Dialog.Description>
-			Create a new goal to track your progress. Fill out the form below to get started.
-		  </Dialog.Description>
-		</Dialog.Header>
-  
-		<!-- Include the GoalForm component here -->
-		<GoalForm data={{ form: data.form }} />
-	  </Dialog.Content>
-	</Dialog.Root>
+		<Dialog.Trigger 
+		  class="px-4 py-2 bg-blue-600 text-white font-semibold rounded-lg shadow-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50 transition duration-300">
+		  + New Goal
+		</Dialog.Trigger>
+		<Dialog.Content>
+		  <Dialog.Header>
+			<Dialog.Title class="text-xl font-bold">New Goal</Dialog.Title>
+			<Dialog.Description class="text-gray-500">
+			  Create a new goal to track your progress. Fill out the form below to get started.
+			</Dialog.Description>
+		  </Dialog.Header>
+	  
+		  <!-- Include the GoalForm component here -->
+		  <GoalForm data={{ form: data.form }} />
+		</Dialog.Content>
+	  </Dialog.Root>
   
 	<DataTable data={data.goals} {columns} />
   </div>
