b5ab487f75ff95744414424e5e6be73e5b9f6684
Josef Kuchař <josef.kuchar267@gmail.com>
2025-02-27 11:44:57 +0100

    dashboard shits

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,6 +6,34 @@
 	import CardHeader from '$lib/components/ui/card/card-header.svelte';
 	import CardTitle from '$lib/components/ui/card/card-title.svelte';
 	import Card from '$lib/components/ui/card/card.svelte';
+	import { PieChart, Text } from 'layerchart';
+
+	const data = [
+		{
+			year: 2019,
+			basket: 1,
+			fruit: 'apples',
+			value: 3840
+		},
+		{
+			year: 2019,
+			basket: 1,
+			fruit: 'bananas',
+			value: 1920
+		},
+		{
+			year: 2019,
+			basket: 2,
+			fruit: 'cherries',
+			value: 960
+		},
+		{
+			year: 2019,
+			basket: 2,
+			fruit: 'grapes',
+			value: 400
+		}
+	];
 </script>
 
 <h1>Welcome to SvelteKit</h1>
@@ -23,3 +51,59 @@
 		<p>Card Footer</p>
 	</CardFooter>
 </Card>
+
+<table class="table-auto">
+	<thead>
+		<tr>
+			<th></th>
+			<th colspan="3">2024</th>
+		</tr>
+		<tr>
+			<th>Cíl</th>
+			<th>1</th>
+			<th>2</th>
+			<th>3</th>
+		</tr>
+	</thead>
+	<tbody>
+		<tr class="table-row">
+			<td><div class="mr-5">Nejaky goal</div></td>
+			<td>
+				<div class="m-[2px] h-7 w-7 rounded bg-green-500"></div>
+			</td>
+			<td>
+				<div class="m-[2px] h-7 w-7 rounded bg-green-500"></div>
+			</td>
+			<td>
+				<div class="m-[2px] h-7 w-7 rounded bg-green-500"></div>
+			</td>
+		</tr>
+		<tr class="table-row">
+			<td><div class="mr-5">Nejaky goal bracho</div></td>
+			<td>
+				<div class="m-[2px] h-7 w-7 rounded bg-green-500"></div>
+			</td>
+			<td>
+				<div class="m-[2px] h-7 w-7 rounded bg-green-500"></div>
+			</td>
+			<td>
+				<div class="m-[2px] h-7 w-7 rounded bg-green-500"></div>
+			</td>
+		</tr>
+	</tbody>
+</table>
+
+<div class="h-[300px] resize overflow-auto rounded border p-4">
+	<PieChart {data} key="fruit" value="value" innerRadius={-20} cornerRadius={5} padAngle={0.02}>
+		<svelte:fragment slot="aboveMarks">
+			<Text value={123} textAnchor="middle" verticalAnchor="middle" class="text-4xl" dy={4} />
+			<Text
+				value="total"
+				textAnchor="middle"
+				verticalAnchor="middle"
+				class="fill-surface-content/50 text-sm"
+				dy={26}
+			/>
+		</svelte:fragment>
+	</PieChart>
+</div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ import tailwindcssAnimate from "tailwindcss-animate";
 /** @type {import('tailwindcss').Config} */
 const config = {
 	darkMode: ["class"],
-	content: ["./src/**/*.{html,js,svelte,ts}"],
+	content: ["./src/**/*.{html,js,svelte,ts}", "./node_modules/layerchart/**/*.{svelte,js}"],
 	safelist: ["dark"],
 	theme: {
 		container: {
