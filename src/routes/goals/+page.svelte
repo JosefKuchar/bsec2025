94ecfc235b11086a9552e1836c2a0ab8b01ae9ab
DaveV6 <bujzasdavid@gmail.com>
2025-02-27 19:20:18 +0100

    add finance calculation for goals

--- src/routes/goals/+page.svelte
+++ src/routes/goals/+page.svelte
@@ -12,9 +12,8 @@
   
   <div class="space-y-4 container">
 	<Dialog.Root>
-		<Dialog.Trigger 
-		  class="px-4 py-2 bg-blue-600 text-white font-semibold rounded-lg shadow-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50 transition duration-300">
-		  + New Goal
+		<Dialog.Trigger>
+		  <Button>+ New Goal</Button>
 		</Dialog.Trigger>
 		<Dialog.Content>
 		  <Dialog.Header>
