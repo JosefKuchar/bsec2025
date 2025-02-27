f86984ea630896ecedb8683b88ff687d85a34b54
Marek324 <hrmajo1@gmail.com>
2025-02-27 13:44:59 +0100

    k

---
+++ src/routes/changes/+page.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+	import type { PageData } from './$types';
+	export let data: PageData;
+	let changes = data.changes;
+	import ChangeModal from '$lib/components/ui/change_modal/ChangeModal.svelte';
+</script>
+
+<svelte:head>
+	<title>Changes</title>
+	<meta name="description" content="List of changes" />
+</svelte:head>
+
+<div class="container mx-auto py-8">
+	<h1 class="mb-4 text-3xl font-bold">Changes</h1>
+
+	<!-- Placeholder for the list -->
+	{#if changes.length === 0}
+		<p>No changes to display.</p>
+	{:else}
+		<ul class="list-inside">
+			{#each changes as change}
+				<li class="mb-2 rounded-lg border shadow-sm transition-shadow duration-200 hover:shadow-md">
+					<div>
+						<ChangeModal {change} />
+					</div>
+				</li>
+			{/each}
+		</ul>
+	{/if}
+</div>
