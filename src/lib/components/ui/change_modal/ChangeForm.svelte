7987e759ff35ec9eddb55fef7df49e3c120f62d4
Marek324 <hrmajo1@gmail.com>
2025-02-27 17:26:20 +0100

    feat: Add ChangeForm component with form and input fields

--- src/lib/components/ui/change_modal/ChangeForm.svelte
+++ src/lib/components/ui/change_modal/ChangeForm.svelte
@@ -6,16 +6,15 @@
 	import { type SuperValidated, type Infer, superForm } from 'sveltekit-superforms';
 	import { zodClient } from 'sveltekit-superforms/adapters';
 	import { createEventDispatcher } from 'svelte';
-	import { Frequency } from '$lib/enums';
 	import * as DropdownMenu from '$lib/components/ui/dropdown-menu/index.js';
-  import { getfrequencyFromString } from '$lib/utils';
 	import prisma from '$lib/prisma';
 
 	const dispatch = createEventDispatcher();
 
 	let {
 		data,
-		change
+		change,
+		types
 	}: {
 		data: { form: SuperValidated<Infer<ChangeFormSchema>> };
 		change: {
@@ -23,9 +22,9 @@
 			from: Date;
 			to: Date;
 			amount: number;
-			frequency: string;
 			type: { id: number; emoji: string; name: string };
 		};
+		types: { id: number; emoji: string; name: string }[];
 	} = $props();
 
 	const form = superForm(data.form, {
@@ -41,7 +40,100 @@
 	const { form: formData, enhance } = form;
 </script>
 
-<form method="POST" use:enhance>
+<form
+	method="POST"
+	onsubmit={(event: any) => {
+		const formData = new FormData(event.target);
+
+		console.log('Form data submitted:', formData);
+
+		fetch('/api/changes', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify(formData)
+		})
+			.then((response) => response.json())
+			.then((data) => {
+				console.log('Success:', data);
+				// closeModal();
+			})
+			.catch((error) => {
+				console.error('Error:', error);
+			});
+	}}
+	use:enhance
+>
+	<Form.Fieldset {form} name="type">
+		<Form.Legend>Type</Form.Legend>
+		<RadioGroup.Root
+			bind:value={$formData.type}
+			class="border-input bg-background ring-offset-background placeholder:text-muted-foreground focus-visible:ring-ring flex h-10 w-full rounded-md border px-3 py-2 text-base file:border-0 file:bg-transparent file:text-sm file:font-medium focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm"
+		>
+			<DropdownMenu.Root>
+				<DropdownMenu.Trigger class="size-full"
+					><div class="flex justify-between">
+						<p>Typ pohybu</p>
+						<sup class="text-xl">⌄</sup>
+					</div></DropdownMenu.Trigger
+				>
+				<DropdownMenu.Content>
+					<DropdownMenu.Group>
+						<DropdownMenu.Item>
+							<Form.Control>
+								{#snippet children({ props })}
+									<RadioGroup.Item value="Příjem" {...props} />
+									<Form.Label class="font-normal">Příjem</Form.Label>
+								{/snippet}
+							</Form.Control>
+						</DropdownMenu.Item>
+						<DropdownMenu.Item>
+							<Form.Control>
+								{#snippet children({ props })}
+									<RadioGroup.Item value="Výdaj" {...props} />
+									<Form.Label class="font-normal">Výdaj</Form.Label>
+								{/snippet}
+							</Form.Control>
+						</DropdownMenu.Item>
+						<DropdownMenu.Item>
+							<Form.Control>
+								{#snippet children({ props })}
+									<RadioGroup.Item value="Dar" {...props} />
+									<Form.Label class="font-normal">Dar</Form.Label>
+								{/snippet}
+							</Form.Control>
+						</DropdownMenu.Item>
+						<DropdownMenu.Item>
+							<Form.Control>
+								{#snippet children({ props })}
+									<RadioGroup.Item value="Potraviny" {...props} />
+									<Form.Label class="font-normal">Potraviny</Form.Label>
+								{/snippet}
+							</Form.Control>
+						</DropdownMenu.Item>
+						<DropdownMenu.Item>
+							<Form.Control>
+								{#snippet children({ props })}
+									<RadioGroup.Item value="Dům" {...props} />
+									<Form.Label class="font-normal">Dům</Form.Label>
+								{/snippet}
+							</Form.Control>
+						</DropdownMenu.Item>
+						<DropdownMenu.Item>
+							<Form.Control>
+								{#snippet children({ props })}
+									<RadioGroup.Item value="Ostatní" {...props} />
+									<Form.Label class="font-normal">Ostatní</Form.Label>
+								{/snippet}
+							</Form.Control>
+						</DropdownMenu.Item>
+					</DropdownMenu.Group>
+				</DropdownMenu.Content>
+			</DropdownMenu.Root>
+		</RadioGroup.Root>
+	</Form.Fieldset>
+
 	<Form.Field {form} name="amount">
 		<Form.Control>
 			{#snippet children({ props })}
@@ -49,46 +141,8 @@
 				<Input {...props} bind:value={$formData.amount} />
 			{/snippet}
 		</Form.Control>
-		<Form.Description>Amount desc</Form.Description>
 		<Form.FieldErrors />
 	</Form.Field>
-
-	<Form.Fieldset {form} name="frequency" class="space-y-3">
-    <Form.Legend>Frequency</Form.Legend>
-    <RadioGroup.Root
-      bind:value={$formData.frequency}
-      class="flex flex-col space-y-1"
-      name="type"
-    >
-      <div class="flex items-center space-x-3 space-y-0">
-        <Form.Control>
-          {#snippet children({ props })}
-            <RadioGroup.Item value="OneTime" {...props} />
-            <Form.Label class="font-normal">Jednorazove</Form.Label>
-          {/snippet}
-        </Form.Control>
-      </div>
-      <div class="flex items-center space-x-3 space-y-0">
-        <Form.Control>
-          {#snippet children({ props })}
-            <RadioGroup.Item value="mentions" {...props} />
-            <Form.Label class="font-normal"
-              >Direction messages and mentions</Form.Label
-            >
-          {/snippet}
-        </Form.Control>
-      </div>
-      <div class="flex items-center space-x-3 space-y-0">
-        <Form.Control>
-          {#snippet children({ props })}
-            <RadioGroup.Item value="none" {...props} />
-            <Form.Label class="font-normal">Nothing</Form.Label>
-          {/snippet}
-        </Form.Control>
-      </div>
-    </RadioGroup.Root>
-    <Form.FieldErrors />
-  </Form.Fieldset>
 
 	<Form.Button>Submit</Form.Button>
 </form>
