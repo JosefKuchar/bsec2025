1fdc90662ee996411cd67ac828200a50f8987d6d
Marek324 <hrmajo1@gmail.com>
2025-02-27 19:17:45 +0100

    a

--- src/lib/components/ui/change_modal/form-schema.ts
+++ src/lib/components/ui/change_modal/form-schema.ts
@@ -1,11 +1,16 @@
-import { z } from "zod";
+import { z } from 'zod';
 
 export const formSchema = z.object({
-  id: z.number().int().positive().optional(), 
-  typeName: z.string(), 
-  amount: z.number(),
-  from: z.date(),
+	id: z.number().int().positive().optional(), // Optional because it's auto-generated
+	amount: z.number(), // Assuming value is a float
+  from: z.date(), // Optional because it's auto-generated
   to: z.date(),
+	type: z.object({
+		name: z.string().min(1).max(255),
+		emoji: z.string().min(1).max(1),
+		id: z.number().int().positive().optional()
+	}), // Assuming name is a string with a max length of 255
+	description: z.string().min(1).max(1000), // Assuming description is a string with a max length of 1000
 });
 
 export type ChangeFormSchema = typeof formSchema;
